403131e69ee278bd259ea601fda3d0114d38fce0
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-07-09 20:54:24 -0300

    desafio tentativas

--- CursoV Pratica/Desafio19.py
+++ CursoV Pratica/Desafio19.py
@@ -1,2 +1,11 @@
 """ Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
 lendo os nomes deles e escrevendo o nome escolhido"""
+import random
+
+n1 = input('Nome do primero aluno:')
+n2 = input('Nome do segundo aluno:')
+n3 = input('Nome do terceiro aluno:')
+n4 = input('Nome do quarto aluno:')
+lista = [n1, n2, n3, n4]
+sort = random.choice(lista)
+print(f'O aluno sorteado para apagar o quadro desta vez é o(a) {sort}')
--- CursoV Pratica/Desafio20.py
+++ CursoV Pratica/Desafio20.py
@@ -1,2 +1,8 @@
 """ O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada"""
+import random
+n1 = input('Aluno :')
+n2 = input('Aluno :')
+n3 = input('Aluno :')
+n4 = input('Aluno :')
+pos = random.random()
