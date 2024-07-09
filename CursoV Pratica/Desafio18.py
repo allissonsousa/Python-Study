9693b488ce595a549956285f645336fb44e38bcb
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-07-08 21:12:11 -0300

    Update Desafio18.py

--- CursoV Pratica/Desafio18.py
+++ CursoV Pratica/Desafio18.py
@@ -1,1 +1,12 @@
-""" Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno cosseno e tangente desse ângulo"""
+""" Faça um programa que leia um ângulo qualquer e mostre na tela o valor
+do seno, cosseno e tangente desse ângulo"""
+from math import cos, sin, tan
+from fractions import Fraction
+ang = int(input('Digite aqui o seu angulo:'))
+c = cos(ang)
+s = sin(ang)
+t = tan(ang)
+print(f'O ângulo de {ang} graus tem como :\n'
+      f'tangente >> {t}\n'
+      f'coseno >> {c}\n'
+      f'seno >> {s}')
