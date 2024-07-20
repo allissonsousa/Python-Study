abaa4bf4788f3713855ee079d5d1937bbd7f375f
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-07-20 00:08:14 -0300

    resoluçao de desafio

--- CursoV Pratica/Desafio23.py
+++ CursoV Pratica/Desafio23.py
@@ -4,10 +4,7 @@ unidades 0
 dezenas 1
 centenas 2
 milhares 3"""
-num = str(input('Digite um numero de 0 a 9999:'))
-num.zfill(3)
-print(f'Milhar: {num[0:1]}')
-print(f'Centenas: {num[1:2]}')
-print(f'Dezenas: {num[2:3]}')
-print(f'Unidades: {num[3:]}')
+nume = str(input('Digite um numero de 0 a 9999:'))
+todos = list(nume)
+print(todos[0])
 """" PARTIR UM NUMERO EM 4 PARTES MESMO QUE VAZIAS"""
--- CursoV Pratica/Desafio27.py
+++ CursoV Pratica/Desafio27.py
@@ -3,3 +3,10 @@ separadamente
 EX >> Ana maria de Sousa
 primeiro Ana
 ultimo sousa"""
+nome = str(input('Digite aqui seu nome completo:'))
+pri = str(nome.split()[0])
+ult = str((nome.split()[::-1])[0])
+print(f'Seu primeiro nome: {pri}\n'
+      f'Seu último nome: {ult}')
+
+"""Uso do ::-1 para inverter a ordem das palavras da frase que ja foi separada pelo split"""
