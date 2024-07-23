20d5d6216ce8e67fdc76c29525fcd6a773717123
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-07-22 21:39:02 -0300

    correção do ultimo desafio por enquanto

--- CursoV Pratica/Desafio27.py
+++ CursoV Pratica/Desafio27.py
@@ -3,6 +3,8 @@ separadamente
 EX >> Ana maria de Sousa
 primeiro Ana
 ultimo sousa"""
+import ntpath
+
 nome = str(input('Digite aqui seu nome completo:'))
 pri = str(nome.split()[0])
 ult = str((nome.split()[::-1])[0])
@@ -10,3 +12,10 @@ print(f'Seu primeiro nome: {pri}\n'
       f'Seu último nome: {ult}')
 
 """Uso do ::-1 para inverter a ordem das palavras da frase que ja foi separada pelo split"""
+
+# CORREÇÃO
+n = str(input('Digite seu nome completo: ')).strip()
+name = n.split()
+print(f'Muito prazer em te conheçer \n'
+      f'Seu primeiro nome é : {name[0]}\n'
+      f'Seu último nome é : {name[len(name)-1]}')
