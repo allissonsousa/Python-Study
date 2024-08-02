1e5050147e3e56145d45aa81d85a0d9ebf348be8
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-01 23:12:19 -0300

    resolução de desafios

--- CursoV Pratica/Desafio44.py
+++ CursoV Pratica/Desafio44.py
@@ -5,7 +5,7 @@ pagamento?
 - em ate 2x no cartao: preço normal
 - 3x ou mais no cartao: 20% de juros"""
 preco = float(input('Qual o valor do produto que você está comprando ? R$'))
-pagamento = int(input('Digite o número de acordo com a sua forma de pagamento: \n'
+pagamento = int(input('Digite o número de acordo com a sua forma de pagamento: \n'  # devia ter usado o '''
                       '1 = A vista no dinheiro\n'
                       '2 = A vista no cartão\n'
                       '3 = Em até 2x no cartão\n'
@@ -19,4 +19,24 @@ elif pagamento == 3:
     valor = preco
 elif pagamento == 4:
     valor = preco + (preco * 10 / 100)
+print(f'O valor a ser pago pelo produto sera de R${valor:.2f}')
+
+# CORREÇÃO
+if pagamento == 1:
+    valor = preco - (preco * 10 / 100)
+elif pagamento == 2:
+    valor = preco - (preco * 5 / 100)
+elif pagamento == 3:
+    valor = preco
+    parcela = preco / 2
+    print(f'Sua compra será parcelada em 2 vezes de R${parcela:.2f}')
+elif pagamento == 4:
+    valor = preco + (preco * 20 / 100)
+    totparc = int(input('Quantas parcelas ?'))
+    parcela = valor / totparc
+    print(f'A sua compra sera parcelada em {totparc} vezes de R${parcela:.2f}')
+else:
+    valor = 0
+    print('\033[1:31mOPÇÃO INVALIDA DE PAGAMENTO, TENTE NOVAMENTE!!\033[m')
+
 print(f'O valor a ser pago pelo produto sera de R${valor:.2f}')
--- CursoV Pratica/Desafio45.py
+++ CursoV Pratica/Desafio45.py
@@ -1,6 +1,7 @@
 """Crie um programa que faça o computador jogar Jokennpo com voce"""
-from random import choice
-n = str(choice(['Tesoura', 'Papel', 'Pedra']).upper())  # lista com os nomes, choice faz o sorteio
+from random import choice, randint
+from time import sleep
+'''n = str(choice(['Tesoura', 'Papel', 'Pedra']).upper())  # lista com os nomes, choice faz o sorteio
 mao = str(input('JO KEN PO!\n').upper())  # o upper usa pra nao dar erro caso o usuario digite de uma forma diferente
 if mao == n:
     print(f'{n}!!!\nQue coincidencia, estamos pensando igual, vamos tentar denovo!')
@@ -11,4 +12,50 @@ elif mao == 'TESOURA' and n == 'PAPEL':
 elif mao == 'PAPEL' and n == 'PEDRA':
     print(f'{n}!!!\nOh não, você mostrou {mao} e ganhou. Parabéns!')
 else:
-    print(f'{n}!!!\nEbaaa, você mostrou {mao} e perdeu. Tente denovo.')
+    print(f'{n}!!!\nEbaaa, você mostrou {mao} e perdeu. Tente denovo.')'''
+
+# CORREÇÃO
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[0] Pedra
+[1] Papel
+[2] Tesoura''')
+jogador = int(input('Qual é a sua jogada?'))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+sleep(0.5)
+print('-=-' * 10)
+print(f'Computador jogou {itens[computador]}')
+print(f'Jogador jogou {itens[jogador]}')
+print('-=-' * 10)
+if computador == 0:
+    if jogador == 0:
+        print('Empate')
+    elif jogador == 1:
+        print('Jogador vence')
+    elif jogador == 2:
+        print('Computador vence')
+    else:
+        print('JOGADA INVÁLIDA')
+elif computador == 1:
+    if jogador == 0:
+        print('Computador vence')
+    elif jogador == 1:
+        print('Empate')
+    elif jogador == 2:
+        print('Jogador vence')
+    else:
+        print('JOGADA INVÁLIDA')
+elif computador == 2:
+    if jogador == 0:
+        print('Jogador vence')
+    elif jogador == 1:
+        print('Computador vence')
+    elif jogador == 2:
+        print('Empate')
+    else:
+        print('JOGADA INVÁLIDA')
