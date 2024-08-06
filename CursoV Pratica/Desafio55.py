82f5b64c7223441b76681aa73c0131211c9ab5cf
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-06 11:58:55 -0300

    Correção de exercicios

--- CursoV Pratica/Desafio55.py
+++ CursoV Pratica/Desafio55.py
@@ -1,1 +1,14 @@
 """Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e menor peso lidos"""
+menor = 0
+maior = 0
+for i in range(1, 6):
+    peso = float(input(f'Peso da {i}° pessoa: '))
+    if i == 1:
+        menor = peso
+        maior = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < peso:
+            menor = peso
+print(f'O maior peso lido foi de {maior}KG e o menor foi de {menor}KG')
--- CursoV Pratica/Desafio56.py
+++ CursoV Pratica/Desafio56.py
@@ -2,3 +2,49 @@
 A media de idade do grupo
 Qual o nome do homem mais velhos
 Quantas mulheres tem menos de 20 anos"""
+velho = 0
+maisve = ''
+menos = 0
+media = 0
+for i in range(1, 5):
+    nome = str(input(f'Nome da {i} pessoa: '))
+    sex = str(input(f'Sexo da {i} pessoa: ')).upper()
+    ida = int(input(f'Idade da {i} pessoa: '))
+    media += ida / 4
+    if sex == 'M' and i == 1:
+        velho = ida
+    else:
+        if ida > velho:
+            velho = ida
+            maisve = str(nome)
+    if ida < 20 and sex == 'F':
+        menos += 1
+print(f'''
+A média de idade desse grupo de pessoas é {int(media)} anos!
+O homem mais velho desse grupo é o {maisve}!
+{menos} Mulheres desse grupo tem menos de vinte anos!''')
+
+# CORREÇÃO
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for i in range(1, 5):
+    print(f'----------------- {i}° PESSOA -------------------------')
+    nome = str(input('NOME: ')).strip()
+    idade = int(input('IDADE: '))
+    sexo = str(input('SEXO [M/F]: ')).strip()
+    somaidade += idade
+    if i == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'fF' and idade < 20:
+        totmulher20 += 1
+    mediadidade = somaidade / 4
+print(f'A média de idade do grupo é de {mediadidade} anos.')
+print(f'O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho}.')
+print(f'Ao todo são {totmulher20} mulheres com menos de 20 anos.')
