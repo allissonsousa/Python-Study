011fe587526032c756bfe36322080e5b175d8ad4
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-08 22:46:32 -0300

    Correção de exercicios

--- CursoV Pratica/Desafio57.py
+++ CursoV Pratica/Desafio57.py
@@ -2,7 +2,13 @@
 peça a digitação novamente, até ter um valor correto"""
 sex = ''
 while sex != 'M' and sex != 'F':
-    sex = str(input('Qual o seu sexo [M/F]: ')).upper()
+    sex = str(input('Qual o seu sexo? [M/F]: ')).upper().strip()
     if sex != 'M' and sex != 'F':
         print('\033[31mValor não aceito!\033[m Digite novamente')
 print('Valor \033[36marmazenado no sistema!\033[m Muito obrigado.')
+
+# CORREÇÃO
+sexo = str(input('Informe o seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print(f'Sexo {sexo} registrado com sucesso.')
--- CursoV Pratica/Desafio58.py
+++ CursoV Pratica/Desafio58.py
@@ -1,6 +1,6 @@
 """ Melhore o jogo do desafio 28 onde o computador vai 'pensar' em um número de 0 a 10 . Só que agora o jogador
 vai tentar adivinhar, até acertar, mostrando no final quantos palpites foram necessários para vencer"""
-from random import choice
+from random import choice, randint
 comp = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 jog = 0
 palp = 0
@@ -12,3 +12,21 @@ while comp != jog:
         print('\033[31mERROU!!!\033[m estou pensando em outro número. Tente novamente!\n')
 print(f'\033[36mPARABÉNS!!!!!\033[m eu estava pensando no número {comp}\n'
       f'Você conseguiu adivinhar só com {palp} tentativas, muito bom.')
+
+# CORREÇÃO
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é o seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente novamente.')
+        elif jogador > computador:
+            print('Menos... Tente novamente')
+print(f'Você acertou com {palpites} tentativas. Parabéns!!')
