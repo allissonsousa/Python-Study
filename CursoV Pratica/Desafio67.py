331586de4030013205cdcdae17bff59750722d60
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-16 23:41:58 -0300

    resoluçao de desafio

--- CursoV Pratica/Desafio67.py
+++ CursoV Pratica/Desafio67.py
@@ -1,5 +1,15 @@
 """Faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo usuário
 O programa sera interrompido quando o  número solicitado foi negativo"""
-print('-=-' * 23)
+n = 1
+while n >= 1:
+    print('-' * 40)
+    n = int(input('Você quer ver a tabuada de qual número? '))
+    print('-' * 40)
+    if n != 0:
+        for i in range(1, 11):
+            print(f'{n} x {i} = {n * i}')
+            i += 1
 
-print('-=-' * 23)
+print('Você digitou um valor não válido. PROGRAMA ENCERRADO!!')
+
+
--- CursoV Pratica/Desafio68.py
+++ CursoV Pratica/Desafio68.py
@@ -1,2 +1,9 @@
 """Faça um programa que jogue par ou impar com o computador. O jog será interrompido quando o jogador perder,
 mostrando o total de vitórias consecutivas que ele conquistou no final do jogo"""
+from random import choice
+arra = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+pc = choice(arra)
+print('._._' * 10)
+print('VAMOS JOGAR IMPAR OU PAR')
+print('._._' * 10)
+jog = 11
