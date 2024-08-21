7f32858dc0ac55f4b2c00cb09fce1bada4b94109
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-20 22:42:43 -0300

    organização das pastas

---
+++ CursoV Pratica/Aulas/Aula16.py
@@ -0,0 +1,9 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')   # tuplas ficam entre parenteses
+# tuplas são imutáveis
+print(len(lanche))
+print(lanche)
+"""for i in lanche:
+    print(i)"""
+
+"""for cont in range(0, len(lanche)):
+    print(lanche[cont])"""
