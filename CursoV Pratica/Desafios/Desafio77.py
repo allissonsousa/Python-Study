bdfb3b8d9adbd8b56268269171425e813c716e49
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-27 22:45:05 -0300

    upadtedesafio77

--- CursoV Pratica/Desafios/Desafio77.py
+++ CursoV Pratica/Desafios/Desafio77.py
@@ -1,2 +1,9 @@
-"""Crie um programa que tenha uma tupla com varias palavras ( nao usar acentos) Depois disso, voce feve mostrar, para
+"""Crie um programa que tenha uma tupla com varias palavras ( nao usar acentos) Depois disso, voce deve mostrar, para
 cada palavra, quais são as suas vogais"""
+tulipa = ('Panda', 'Mercado', 'Pedra', 'Comercial', 'Mexirica', 'Corda', 'Motocross', 'Roupas')
+for i in range(0, len(tulipa)):
+    palavra = str(tulipa[i])
+    print(palavra)
+    for c in range(0, len(palavra)):
+        if palavra[c] in 'AaEeIiOoUu':
+            print(palavra[c])
