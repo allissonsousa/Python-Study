a9cff90f57e7df15b2ed757c59a392a20cce90c8
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-09-03 21:36:08 -0300

    Desafio82Solução

--- CursoV Pratica/Desafios/Desafio82.py
+++ CursoV Pratica/Desafios/Desafio82.py
@@ -1,3 +1,19 @@
 """ Crie um programa que vai ler vários números e coocar em uma lista. Depois disso, crie duas listas extras que vão
 conter apenas os valores pares e os valores impares digitados respectivamente.
 No final mostre o conteúdo das tres listas geradas"""
+completa = []
+pares = []
+impares = []
+cont = 'S'
+while cont == 'S':
+    n = int(input('Digite um número: '))
+    completa.append(n)
+    cont = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        impares.append(n)
+
+print(f'''A lista completa é {completa}
+A lista de pares é {pares}
+A lista de impares é {impares}''')
