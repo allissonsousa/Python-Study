5f3f44f03bc7b920a30266376dce966a95d37f6e
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-09-03 22:34:42 -0300

    desafio78Solução

--- CursoV Pratica/Desafios/Desafio83.py
+++ CursoV Pratica/Desafios/Desafio83.py
@@ -1,2 +1,13 @@
 """Crie um programa onde o usuário digite uma expressão qualquer que use parenteses. Seu aplicativo deverá analisar
 se a expressão passada esta com parenteses abertos e fechados na ordem correta"""
+exp = str(input('Digite uma expressão: ')).strip()
+ls = []
+for i in exp:
+    ls.append(i)
+
+if ls.index('(') < ls.index(')') and ls.count('(') == ls.count(')') and ls[len(ls) - 1] != '(':
+    print('Sua expressão está correta')
+else:
+    print('Sua expressão está errada')
+
+
