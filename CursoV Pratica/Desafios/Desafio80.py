ffd111daa4c87e2770d99971c9fd683b03991aa3
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-09-03 21:13:46 -0300

    desafio80Solução

--- CursoV Pratica/Desafios/Desafio80.py
+++ CursoV Pratica/Desafios/Desafio80.py
@@ -1,2 +1,14 @@
 """Crie um programa onde o usuário possa digitar cinco  valores numéricos e cadastre-os em uma lista, já na posição
 correta de inserção ( sem usar o metodo, sort(). No final, mostre a lista ordenada na tela"""
+ls = []
+for i in range(0, 5):
+    n = int(input('Digite um valor: '))
+    ls.append(n)
+
+for i in range(0, len(ls)):
+    minindex = i
+    for d in range(minindex, len(ls)):
+        if ls[minindex] > ls[d]:
+            (ls[minindex], ls[d]) = (ls[d], ls[minindex])
+print('=x=' * 20)
+print(f'Os valores digitados em ordem foram: {ls}')
