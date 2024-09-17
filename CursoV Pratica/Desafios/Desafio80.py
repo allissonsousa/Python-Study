287260bf7c2a14425650d5296369d39da00bf613
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-09-16 22:41:30 -0300

    desafio80Correção

--- CursoV Pratica/Desafios/Desafio80.py
+++ CursoV Pratica/Desafios/Desafio80.py
@@ -15,4 +15,20 @@ print(f'Os valores digitados em ordem foram: {ls}')
 
 # CORREÇÃO
 
+lista = list()
+for c in range(0,5):
+    num = int(input('Digite um valor:'))
+    if c == 0 or num > lista[-1]:
+        lista.append(num)
+        print('O valor foi adicionado no fim da lista')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                print(f'Adicionado na posição {pos} da lista')
+                break
+            pos += 1
 
+print('-=-' * 30)
+print(f'Os valores digitados foram {lista}')
