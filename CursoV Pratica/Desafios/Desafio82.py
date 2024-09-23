401916e27dea2f2b8d694266cb6488c5a582ac81
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-09-23 10:57:00 -0300

    CorreçãoDesafio81

--- CursoV Pratica/Desafios/Desafio82.py
+++ CursoV Pratica/Desafios/Desafio82.py
@@ -1,4 +1,4 @@
-""" Crie um programa que vai ler vários números e coocar em uma lista. Depois disso, crie duas listas extras que vão
+""" Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão
 conter apenas os valores pares e os valores impares digitados respectivamente.
 No final mostre o conteúdo das tres listas geradas"""
 completa = []
@@ -17,3 +17,22 @@ while cont == 'S':
 print(f'''A lista completa é {completa}
 A lista de pares é {pares}
 A lista de impares é {impares}''')
+
+# CORREÇÃO
+par = list()
+imp = list()
+num = []
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'nN':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        par.append(v)
+    else:
+        imp.append(v)
+print('=-=' * 30)
+print(f'Lista : {num}')
+print(f'Pares : {par}')
+print(f'Impares : {imp}')
