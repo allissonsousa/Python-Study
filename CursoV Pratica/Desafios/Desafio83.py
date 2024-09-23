107ba3c4fef58096a26b0d4edeedd3fefadf3999
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-09-23 11:17:06 -0300

    CorreçãoDesafio83

--- CursoV Pratica/Desafios/Desafio83.py
+++ CursoV Pratica/Desafios/Desafio83.py
@@ -11,3 +11,20 @@ else:
     print('Sua expressão está errada')
 
 
+# CORREÇÃO
+expressao = str(input('Digite a expressão: '))
+pilha = []
+for simb in expressao:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está correta! ')
+else:
+    print('Sua expressão está errada!')
+
