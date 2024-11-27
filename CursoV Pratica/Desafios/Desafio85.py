260373553142404fc38957489750663a96369177
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-11-26 22:21:13 -0300

    CorreçãoEx85

--- CursoV Pratica/Desafios/Desafio85.py
+++ CursoV Pratica/Desafios/Desafio85.py
@@ -1,2 +1,35 @@
 """Crie um programa onde o usuario possa digitar sete valores numericos e cadastre-os em uma lista unica que mantenha
 separado os valores pares e impares. No final, mostre os valores pares e impares em ordem crescente"""
+par = []
+imp = []
+todo = []
+for i in range(1, 8):
+    number = int(input(f'Digite o {i}° valor:'))
+    if number % 2 == 0:
+        par.append(number)
+    else:
+        imp.append(number)
+todo.append(par)
+todo.append(imp)
+
+
+
+print('-=-' * 13)
+print(f'Os valores pares digitados foram: {sorted(todo[0])}\n'
+      f'Os valores impares digitados foram: {sorted(todo[1])}')
+
+#----------------
+# Correção
+num = [[],[]]
+for c in range(1, 8):
+    val = int(input(f'Digite o {0}° valor: '))
+    if val % 2 == 0:
+        num[0].append(val)
+    else:
+        num[1].append(val)
+
+print('-=-' * 12)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares digitados foram: {num[0]}')
+print(f'Os valres impares digitados foram: {num[1]}')
--- CursoV Pratica/Desafios/Desafio86.py
+++ CursoV Pratica/Desafios/Desafio86.py
@@ -1,2 +1,2 @@
-"""Crie um programa que cire uma matraiz de dimensao 3x3 e preencha com valores lidos pelo teclado
+"""Crie um programa que crie uma matriz de dimensao 3x3 e preencha com valores lidos pelo teclado
 No final mostre a matriz na tela, com a formataçao correta"""
