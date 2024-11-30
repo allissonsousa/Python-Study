f0b805e2a2aa72f0c8c6af69c67a530f33ec6066
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-11-30 00:15:06 -0300

    Desafio88

--- CursoV Pratica/Desafios/Desafio88.py
+++ CursoV Pratica/Desafios/Desafio88.py
@@ -1,2 +1,23 @@
 """Faça um programa que ajuda um jogador da Mega sena a criar palpites. O programa vai perguntar quantos jogos serão
-gerados e vai sortear 6 números entre 1 e 60 oara cada jogo, cadastrando tudo em uma lista composta"""
+gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta"""
+from random import randint
+jogo = []
+composta = []
+print('==' * 20)
+print(f'APOSTA DO TIGRINHO'.center(40))
+print('==' * 20)
+quantos = int(input('Quantos jogos você quer que eu sorteie? '))
+print(f'SORTEANDO {quantos} JOGOS'.center(40, '='))
+for i in range(0, quantos):
+    while len(jogo) < 6:
+        val = randint(1, 60)
+        if val not in jogo:
+            jogo.append(val)
+    composta.append(jogo[:])
+    jogo.clear()
+
+for i in range(0, quantos):
+    print(f'JOGO {i + 1}: {composta[i]}')
+
+print('-=-' * 10)
+
