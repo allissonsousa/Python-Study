dd8286e554130234092ae0c486a381cf03f13b61
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-11-29 23:25:46 -0300

    CorreçãoD87

--- CursoV Pratica/Desafios/Desafio86.py
+++ CursoV Pratica/Desafios/Desafio86.py
@@ -1,2 +1,37 @@
 """Crie um programa que crie uma matriz de dimensao 3x3 e preencha com valores lidos pelo teclado
 No final mostre a matriz na tela, com a formataçao correta"""
+from os.path import split
+x = ''
+matriz = [[],[],[]]
+for l in range(0,3):
+    for c in range(0,3):
+        valor = [int(input(f'Digite um valor para [{l},{c}]: '))]
+        matriz[l].append(valor)
+
+print('-=-' * 12)
+
+for m in range(0,3):
+    for h in range(0,3):
+        if h != 2:
+            x = ' '
+        else:
+            x = '\n'
+        print(matriz[m][h],end = x)
+
+
+#---------------------------------------
+# CORREÇÃO
+
+mat = [[0,0,0],[0,0,0],[0,0,0]]
+for li in range(0,3):
+    for cas in range(0,3):
+        mat[li][cas] = int(input(f'Digite um valor para [{li}], [{cas}]: '))
+
+print('-=-' * 30)
+for li in range(0,3):
+    for cas in range(0,3):
+        print(f'[{mat[li][cas]:^5}',end=' ')
+    print()
+
+
+
--- CursoV Pratica/Desafios/Desafio87.py
+++ CursoV Pratica/Desafios/Desafio87.py
@@ -2,3 +2,31 @@
 a soma de todos os valores pares digitados
 a soma dos valores da terceira coluna
 o maior valor da segunda linha"""
+soma = terceira = maior = 0
+matriz = [[],[],[]]
+for l in range(0,3):
+    for c in range(0,3):
+        valor = int(input(f'Digite um valor para [{l}, {c}]: '))
+        matriz[l].append(valor)
+        if valor % 2 == 0:
+            soma += matriz[l][c]
+
+        if c == 2:
+            terceira += valor
+
+        if l == 1:
+            if valor > maior:
+                maior = valor
+
+
+print('-=-' * 30)
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'{matriz[l][c]:^5}', end='')
+    print()
+
+print('-=-' * 30)
+print(f'A soma dos valores pares é {soma}\n'
+      f'A soma dos valores da terceira coluna é {terceira}\n'
+      f'O maior valor da segunda linha é {maior}')
