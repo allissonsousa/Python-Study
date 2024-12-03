c2f68b2bce9df1b20adcbc1ff654c4fe1139ce5e
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-12-02 22:41:40 -0300

    resoluçaoDesafio88E89

--- CursoV Pratica/Desafios/Desafio88.py
+++ CursoV Pratica/Desafios/Desafio88.py
@@ -1,6 +1,8 @@
 """Faça um programa que ajuda um jogador da Mega sena a criar palpites. O programa vai perguntar quantos jogos serão
 gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta"""
 from random import randint
+from time import sleep
+
 jogo = []
 composta = []
 print('==' * 20)
@@ -13,11 +15,13 @@ for i in range(0, quantos):
         val = randint(1, 60)
         if val not in jogo:
             jogo.append(val)
+    jogo.sort()
     composta.append(jogo[:])
     jogo.clear()
 
 for i in range(0, quantos):
     print(f'JOGO {i + 1}: {composta[i]}')
+    sleep(0.7)
 
 print('-=-' * 10)
 
--- CursoV Pratica/Desafios/Desafio89.py
+++ CursoV Pratica/Desafios/Desafio89.py
@@ -1,2 +1,34 @@
 """Crie um programa que leia nome e duas notas e guarde tudo em uma lista composta. No final, mostre um boletim
 contendoa  média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente"""
+
+aluno = []
+geral = []
+cont = 0
+while True:
+    nome = str(input('Nome: '))
+    n1 = int(input('Nota 1: '))
+    n2 = int(input('Nota 2: '))
+    continua = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    aluno = [nome, n1, n2]
+    geral.append(aluno[:])
+    aluno.clear()
+    cont += 1
+    if continua == 'N':
+        break
+
+print('=-=' * 16)
+print(f'No°  NOME          MÉDIA')
+print('--' * 15)
+for i in range(0, len(geral)):
+    media = (geral[i][1] + geral[i][2]) / 2
+    print(f'{i}   {geral[i][0]}                 ',end = f'{media}\n')
+print('--' * 20)
+
+while True:
+    mostrar = int(input('Mostrar notas de qual aluno? (999 interrompe):  '))
+    if mostrar == 999:
+        break
+
+    print(f'As notas de {geral[mostrar][0]} são: [{geral[mostrar][1]}, {geral[mostrar][2]}]')
+
+    print('--' * 20)
