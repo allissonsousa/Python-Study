1c62c95a7b75cc8ec216a2ae6c64de2bcc7e7c55
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-12-04 22:41:01 -0300

    desafio90

--- CursoV Pratica/Desafios/Desafio90.py
+++ CursoV Pratica/Desafios/Desafio90.py
@@ -1,2 +1,5 @@
 """Faça um programa que leia nome e média de um aluno, guardando tambem a situção em um dicionário. No final, mostre o
 conteúdo da estrutura na tela"""
+media = nome = situacao = ''
+dados = {'nome' : nome, 'media' : media, 'situação' : situacao}
+print(dados)
