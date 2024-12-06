1c46b1002e20d856cd62393bf3f485fca644f7e5
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-12-05 22:29:10 -0300

    exercicio91

--- CursoV Pratica/Desafios/Desafio90.py
+++ CursoV Pratica/Desafios/Desafio90.py
@@ -1,5 +1,32 @@
 """Faça um programa que leia nome e média de um aluno, guardando tambem a situção em um dicionário. No final, mostre o
 conteúdo da estrutura na tela"""
-media = nome = situacao = ''
+
+situacao = ''
+nome = str(input('Nome: '))
+media = float(input('Média: '))
+if media >= 6:
+    situacao = 'Aprovado'
+elif media < 6:
+    situacao = 'Reprovado'
 dados = {'nome' : nome, 'media' : media, 'situação' : situacao}
-print(dados)
+print('-=-' * 30)
+print(f'O nome é igual a {dados['nome']}\n'
+      f'A média é igual a {dados['media']}\n'
+      f'A Situação é igual a {dados['situação']}')
+
+#---------------------------------------
+#Correção
+
+aluno =  dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5<= aluno['média'] <7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+
+print('-=-' * 30)
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}')
--- CursoV Pratica/Desafios/Desafio91.py
+++ CursoV Pratica/Desafios/Desafio91.py
@@ -1,2 +1,25 @@
-"""Crie um programa on de 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
-dicionário, no final, coloque esse dicionpario em ordem, sabendo que o vencedor tirou o maior número dado"""
+"""Crie um programa de 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
+dicionário, no final, coloque esse dicionario em ordem, sabendo que o vencedor tirou o maior número dado"""
+from time import sleep
+from random import randint
+jogadores = list()
+jogador = dict()
+contador = list()
+
+print('Valores sorteados: \n')
+for i in range(1, 5):
+    dado = randint(1, 6)
+    print(f'O jogador {i} tirou {str(dado)} no dado')
+    jogador['jogador'] = f'jogador{i}'
+    jogador['dado'] = dado
+    jogadores.append(jogador.copy())
+    sleep(0.7)
+for i in range(len(jogadores)):
+    contador.append(jogadores[i]['dado'])
+contador.sort()
+print(contador)
+for c in range(len(jogadores)):
+    for i in range(0, 4):
+        if jogadores[i]['dado'] == contador[i]:
+            print(f'{i}° lugar:{jogadores[i]['jogador']}')
+
