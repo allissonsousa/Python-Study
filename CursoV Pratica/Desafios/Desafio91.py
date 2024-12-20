28905e984f19e776eb0d46092dad2adaf154eb6e
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2024-12-19 22:22:06 -0300

    desafio 91

--- CursoV Pratica/Desafios/Desafio91.py
+++ CursoV Pratica/Desafios/Desafio91.py
@@ -2,6 +2,8 @@
 dicionário, no final, coloque esse dicionario em ordem, sabendo que o vencedor tirou o maior número dado"""
 from time import sleep
 from random import randint
+from operator import itemgetter
+"""
 jogadores = list()
 jogador = dict()
 contador = list()
@@ -14,12 +16,40 @@ for i in range(1, 5):
     jogador['dado'] = dado
     jogadores.append(jogador.copy())
     sleep(0.7)
+
+print(jogadores)
 for i in range(len(jogadores)):
     contador.append(jogadores[i]['dado'])
 contador.sort()
 print(contador)
 for c in range(len(jogadores)):
-    for i in range(0, 4):
+    for i in range(1, 5):
         if jogadores[i]['dado'] == contador[i]:
             print(f'{i}° lugar:{jogadores[i]['jogador']}')
+            jogadores.remove(f'jogador{i}')
+            jogadores.remove('dado')"""
 
+# Não consegui resolver F
+
+#---------------------------------------------
+#Correção
+
+jogo = {'jogador1' : randint(1, 6),
+        'jogador2' : randint(1, 6),
+        'jogador3' : randint(1, 6),
+        'jogador4' : randint(1, 6)}
+
+ranking = list()
+
+print('Valores sorteados: ')
+for k, v in jogo.items():
+        print(f'{k} tirou {v} no dado')
+        sleep(0.7)
+
+#IMPORTANTE !!!!!!
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)   #itemgetter 1 pega o elemento 1 do dicionario, q é o valor
+                                                        # de jogador, item 0 no caso seria 'jogador', uso da chave
+
+print('-=-' * 30)
+for i, v in enumerate(ranking):
+        print(f'{i + 1}° Lugar: {v[0]} com {v[1]}')
--- CursoV Pratica/Desafios/Desafio92.py
+++ CursoV Pratica/Desafios/Desafio92.py
@@ -2,3 +2,8 @@
 se por acaso a CTPS for diferente de ZERO, o dicionário recebera tambem o ano de contratação e o salãrio. Calcule e
 acrescente, além da idade, com quantos anos a pessoa vai se aposentar. Leve em conta que sao 35 de contribuição
 para se aposentar"""
+
+dados = {}
+nome = str(input('Nome: '))
+ano = int(input('Ano de nascimento: '))
+carteira = int(input('Carteira de trabalho: '))
