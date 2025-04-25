cab693b386e7c71ba30f2494a4cc2045038a890e
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-04-24 23:57:49 -0300

    dados

--- CursoADSpratica/Analise de Dados/Medidas de dispersão.py
+++ CursoADSpratica/Analise de Dados/Medidas de dispersão.py
@@ -1,5 +1,10 @@
 """Medidas de variabilidades ou dispersão"""
 # quao dispersos estão os dados em relação ao valor da media
-# variancia, desvio padrão, amplitude, coeficiencia de variação e desvio médio absoluto
+# variancia, desvio padrão, amplitude, quartil, coeficiencia de variação e desvio médio absoluto
 #  statitics.variance
 # statitics.stdev --- desvio padrão
+# numpy para amplitude .ptp
+# quartil numpy.percentile [25,50,75]  25- primeiro quartil, 50 mediana, 75 quartil
+
+# a função describe() retorna as estatisticas dos dados com todas as medidas citadas anteriormente
+
--- CursoADSpratica/Analise de Dados/Medidas de posição.py
+++ CursoADSpratica/Analise de Dados/Medidas de posição.py
@@ -1,5 +1,15 @@
 """Medidas de posiçao ou tendencia central"""
 #media, moda, mediana
-import matplotlib ,statistics ,numpy, pandas
+import matplotlib ,statistics ,numpy as np, pandas
 
-# o statics tem o .mediana o .mode. e o .media
+# o statics tem o .median o .mode. e o .mean
+
+mil = [58, 64, 58, 56, 68, 55, 68, 62, 68, 50, 54, 70]
+media = statistics.mean(mil)
+mediana =  statistics.median(mil)
+frequencia = statistics.variance(mil, 68)
+
+# frequencia, de todos os numeros q apareceram na sequencia SEM REPETIÇAO qual porçao esse valor tem aparecido
+
+
+print(media, mediana, frequencia)
