8f34dfe10b9eae79b7025f3bb44f9f57696243a0
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-04-24 23:07:24 -0300

    estudosdados

---
+++ CursoADSpratica/Analise de Dados/Medidas de dispersão.py
@@ -0,0 +1,5 @@
+"""Medidas de variabilidades ou dispersão"""
+# quao dispersos estão os dados em relação ao valor da media
+# variancia, desvio padrão, amplitude, coeficiencia de variação e desvio médio absoluto
+#  statitics.variance
+# statitics.stdev --- desvio padrão
---
+++ CursoADSpratica/Analise de Dados/Medidas de posição.py
@@ -0,0 +1,5 @@
+"""Medidas de posiçao ou tendencia central"""
+#media, moda, mediana
+import matplotlib ,statistics ,numpy, pandas
+
+# o statics tem o .mediana o .mode. e o .media
