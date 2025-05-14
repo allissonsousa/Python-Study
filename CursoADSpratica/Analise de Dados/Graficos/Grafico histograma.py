0a14b65ad060a654b9497a317d49733b6787fe54
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-05-14 16:42:52 -0300

    graficos 2

--- CursoADSpratica/Analise de Dados/Graficos/Grafico histograma.py
+++ CursoADSpratica/Analise de Dados/Graficos/Grafico histograma.py
@@ -1,10 +1,13 @@
 # no contexto de estatisticas, o histograma é nada mais q o grafico da função de probabilidade
-import panda as pd, matplotlib.pyplot as plt
+import pandas as pd, matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
 #carregando o conjunto de dados california housing
 data = fetch_california_housing()
 #transformando em um DataFrame
 df = pd.DataFrame(data.data, columns=data.feature_names)
 
-#gerando o histograma
-plt.hist(df["MedInc"])
+#gerando o histograma da variavel medinc
+plt.hist(df["MedInc"], bins= 50)
+plt.xlabel("MedInc")
+plt.ylabel("frequencia")
+plt.show()
