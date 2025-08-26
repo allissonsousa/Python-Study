bb7b4a2a270bd3a524678cca3ff64799007d1e31
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-08-26 17:00:02 -0300

    Update AbasPorMesAno.py

--- Estagio/UmaPlanilhaEmAbasMesAno/AbasPorMesAno.py
+++ Estagio/UmaPlanilhaEmAbasMesAno/AbasPorMesAno.py
@@ -4,4 +4,8 @@ from pathlib import Path
 import re
 
 #Nomes dos meses em Portugues para nomear cada aba do arquivo
-MONTH_NAMES_PT = 
+MONTH_NAMES_PT = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+]
+def sanitize_sheet_name(sheet_name):
